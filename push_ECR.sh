fb426aea236c135457754c868bb28daf757f9cf0
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-16 11:45:31 +0700

    Update push_ECR.sh

--- push_ECR.sh
+++ push_ECR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "PUSH IMAGE TO REGISTRY"
-docker login -u manhnh -p $PASSWORD
+docker login -u manhnh -p ${env.PASSWORD}
 docker tag nodejs:$IMAGE-$BUILD_NUMBER manhnh1995/nodejs:$IMAGE-$BUILD_NUMBER
 docker push manhnh1995/nodejs:$IMAGE-$BUILD_NUMBER
